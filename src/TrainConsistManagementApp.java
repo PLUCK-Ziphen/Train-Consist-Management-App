9ff1a3e7e933b554428bef06f931a075b904871c
PLUCK-Ziphen <akhilesheerni@gmail.com>
2026-04-15 14:33:45 +0530

    UC5: Preserve Insertion Order of Bogies

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,18 +1,17 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedHashSet;
 
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
         System.out.println("=== Train Consist Management App ===");
 
-        Set<String> bogieIds = new HashSet<>();
+        LinkedHashSet<String> trainFormation = new LinkedHashSet<>();
 
-        bogieIds.add("BG101");
-        bogieIds.add("BG102");
-        bogieIds.add("BG103");
-        bogieIds.add("BG101");
-        bogieIds.add("BG102");
+        trainFormation.add("Engine");
+        trainFormation.add("Sleeper");
+        trainFormation.add("Cargo");
+        trainFormation.add("Guard");
+        trainFormation.add("Sleeper");
 
-        System.out.println("Unique Bogie IDs: " + bogieIds);
+        System.out.println("Final Train Formation: " + trainFormation);
     }
 }
